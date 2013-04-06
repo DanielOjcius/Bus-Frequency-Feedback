364abe093dc9d96fee1f0fd7b9f56190af1eab87
deepthi <deepthirera@gmail.com>
2013-04-06 15:23:58 +0530

    Deepthi | Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BffService::Application.config.secret_token = '9534dbb841ccf2c17a46d62352ac53e41d1bd57316f1d3b2e0a1765a3217d11c3c750b3b6ba1ba11cc6753f48bfa5e837ebc11baee65dc50a946fd585b53d158'
