d6436dfe3819cbfc1f447b505cf9f86edcee17e5
deepthi <deepthirera@gmail.com>
2013-04-06 21:49:59 +0530

    Deepthi | not found error message

--- app/controllers/bus_frequency_controller.rb
+++ app/controllers/bus_frequency_controller.rb
@@ -3,11 +3,12 @@ class BusFrequencyController < ApplicationController
 		frequency = params[:frequency]
 		bus_frequency = BusFrequency.where(:bus_no => params[:bus_no], :time_range => params[:range]).first_or_create
 		bus_frequency.update_attribute(frequency, bus_frequency.send(frequency) + 1)
-		render :text => 'registered'
+		render :text => 'registered', :status => 200
 	end
 
 	def bus_number
 		bus_frequency = BusFrequency.find_by_bus_no(params[:bus_no])
+		render :text => 'not found' and return if bus_frequency.nil?
 		render :json => bus_frequency.attributes.except("id", "created_at", "updated_at").to_json
 	end
 end
